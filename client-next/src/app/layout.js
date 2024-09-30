fe01d14683e0a74dbfc0c6ea106ff2c1448d33a3
Ruler10123 <ruler10123@tamu.edu>
2024-09-29 23:59:22 -0500

    base page design

--- client-next/src/app/layout.js
+++ client-next/src/app/layout.js
@@ -1,15 +1,19 @@
 import "./globals.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const metadata = {
   title: "VCT Hackathon",
-  description: "LLM model for VCT Hackathon 2024",
+  description: "LLM-Powered Esports Assistant for VCT Hackathon 2024",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
