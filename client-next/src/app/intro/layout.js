1b76fa27eace2bafc98a14e093d4335aa7f215ee
Nathan Foo <ntfoo27@outlook.com>
2024-10-01 00:01:21 -0500

    Remove metadata and extraneous imports

--- client-next/src/app/intro/layout.js
+++ client-next/src/app/intro/layout.js
@@ -1,10 +1,3 @@
-import "../globals.css";
-
-export const metadata = {
-  title: "VCT Hackathon",
-  description: "LLM-Powered Esports Assistant for VCT Hackathon 2024",
-};
-
 export default function IntroLayout({ children }) {
   return (
     <div>
